4d123becfd99b7d15f87d40286bf4f4e3acc2129
Stephanie McNiel <charliePro@CharliePro.local>
2016-04-05 17:59:10 -0400

    stand-in code to finish

---
+++ Assignment2/salaries.py
@@ -0,0 +1,185 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+%matplotlib inline
+
+sal = pd.read_csv('./data/salaries-mergedv2.csv')
+
+# eliminate rows with missing Y values (NaN)
+sal['missingSalary'] = pd.isnull(sal['salary'])
+sal2 = sal[(sal.missingSalary == False)]
+
+# list of X vars to include
+X_numeric_features = ['sup1', 'sup2', 'sup3', 'sup4', 'sup5', 'disabled', 
+					  'yearsInPosition', 'yearsInProfession', 'age', 'cred1', 
+					  'cred2', 'inst1', 'inst2', 'inst3', 'inst4', 'inst5', 
+					  'instBudget', 'instSize', 'pop']
+X_numeric = sal2[X_numeric_features]
+X_categorical_features = ['Function', 'gender', 'race', 'highestDegree', 'Category', 'instType']
+X_categorical = sal2[X_categorical_features]
+
+# create dummy variables for each of the categorical features
+# DOC: http://pandas.pydata.org/pandas-docs/stable/generated/pandas.get_dummies.html
+
+Function_dummies = pd.get_dummies(X_categorical['Function'])
+gender_dummies = pd.get_dummies(X_categorical['gender'])
+race_dummies = pd.get_dummies(X_categorical['race'])
+highestDegree_dummies = pd.get_dummies(X_categorical['highestDegree'])
+Category_dummies = pd.get_dummies(X_categorical['Category'])
+instType_dummies = pd.get_dummies(X_categorical['instType'])
+
+X_dummy_features = pd.concat([Function_dummies, gender_dummies, race_dummies, highestDegree_dummies, Category_dummies, instType_dummies], axis=1)
+
+# convert to ndarray
+X_dummy_features = X_dummy_features.as_matrix()
+
+# impute missing values in numerical features
+# DOC: http://scikit-learn.org/stable/modules/preprocessing.html
+
+from sklearn.preprocessing import Imputer
+imp = Imputer()
+imp.fit(X_numeric)
+X_numeric_imputed = imp.transform(X_numeric)
+
+X = np.concatenate((X_dummy_features, X_numeric_imputed), axis=1)
+
+# y is salary
+y = sal2.iloc[:, 6].values
+
+# create training and test sets
+from sklearn.cross_validation import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+         X, y, test_size=0.3, random_state=0)
+
+# keep track of variance on test data, to graph
+var_to_graph = {}
+# bring residual sum of squares from regression1.ipynb
+var_to_graph['simpReg'] = 265376883.08
+
+from sklearn import datasets, linear_model
+# DOC: http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html
+
+# Create linear regression object
+regr = linear_model.LinearRegression()
+
+# Train the model using the training sets
+X_train_no_intercept = X_train
+X_train = X_train.reshape(-1, X_train.shape[1])
+regr.fit(X_train, y_train)
+
+# The intercept
+print('Intercept: \n', regr.intercept_)
+# The coefficients
+print('Coefficients: \n', regr.coef_)
+# The mean square error
+print("Residual sum of squares, training data: %.2f"
+      % np.mean((regr.predict(X_train) - y_train) ** 2))
+print("Residual sum of squares, test data: %.2f"
+      % np.mean((regr.predict(X_test) - y_test) ** 2))
+var_to_graph['multReg_linear'] = np.mean((regr.predict(X_test) - y_test) ** 2)
+# Explained variance score: 1 is perfect prediction
+print('Variance score, training data: %.2f' % regr.score(X_train, y_train))
+#vector of prediction error
+print('Distribution of prediction error on training data:')
+predError = regr.predict(X_train) - y_train
+plt.hist(predError)
+plt.show()
+
+print('Distribution of prediction error on test data:')
+predError = regr.predict(X_test) - y_test
+plt.hist(predError)
+plt.show()
+
+from sklearn.preprocessing import PolynomialFeatures
+# DOC: http://scikit-learn.org/stable/modules/preprocessing.html
+
+poly = PolynomialFeatures(2)
+X_poly = poly.fit_transform(X)
+
+# create training and test sets
+from sklearn.cross_validation import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+         X_poly, y, test_size=0.3, random_state=0)
+
+## POLYNOMINAL 
+# Create linear regression object
+poly = linear_model.LinearRegression(normalize=True)
+
+# Train the model using the training sets
+X_train_no_intercept = X_train
+X_train = X_train.reshape(-1, X_train.shape[1])
+poly.fit(X_train, y_train)
+
+# The intercept
+print('Intercept: \n', poly.intercept_)
+# The coefficients
+print('Coefficients: \n', poly.coef_)
+# The mean square error
+print("Residual sum of squares, training data: %.2f"
+      % np.mean((poly.predict(X_train) - y_train) ** 2))
+print("Residual sum of squares, test data: %.2f"
+      % np.mean((poly.predict(X_test) - y_test) ** 2))
+var_to_graph['multReg_poly'] = np.mean((poly.predict(X_test) - y_test) ** 2)
+# Explained variance score: 1 is perfect prediction
+print('Variance score, training data: %.2f' % poly.score(X_train, y_train))
+#vector of prediction error
+print('Distribution of prediction error on training data:')
+predError = poly.predict(X_train) - y_train
+plt.hist(predError)
+plt.show()
+
+print('Distribution of prediction error on test data:')
+predError = poly.predict(X_test) - y_test
+plt.hist(predError)
+plt.show()
+
+## RIDGE REGRESSION
+# DOC: http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.Ridge.html
+
+# create training and test sets
+from sklearn.cross_validation import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+         X, y, test_size=0.3, random_state=0)
+
+# Create linear regression object
+regr = linear_model.Ridge()
+
+# Train the model using the training sets
+X_train_no_intercept = X_train
+X_train = X_train.reshape(-1, X_train.shape[1])
+regr.fit(X_train, y_train)
+
+# The intercept
+print('Intercept: \n', regr.intercept_)
+# The coefficients
+print('Coefficients: \n', regr.coef_)
+# The mean square error
+print("Residual sum of squares, training data: %.2f"
+      % np.mean((regr.predict(X_train) - y_train) ** 2))
+print("Residual sum of squares, test data: %.2f"
+      % np.mean((regr.predict(X_test) - y_test) ** 2))
+var_to_graph['multReg_ridge'] = np.mean((regr.predict(X_test) - y_test) ** 2)
+# Explained variance score: 1 is perfect prediction
+print('Variance score, training data: %.2f' % regr.score(X_train, y_train))
+#vector of prediction error
+print('Distribution of prediction error on training data:')
+predError = regr.predict(X_train) - y_train
+plt.hist(predError)
+plt.show()
+
+print('Distribution of prediction error on test data:')
+predError = regr.predict(X_test) - y_test
+plt.hist(predError)
+plt.show()
+
+
+# bar graph of dict with Residual Sum of Squares on test datasets
+
+#var_to_graph['multReg_poly'] = 0
+plt.bar(range(len(var_to_graph)), var_to_graph.values(), align='center')
+plt.xticks(range(len(var_to_graph)), var_to_graph.keys())
+
+plt.show()
